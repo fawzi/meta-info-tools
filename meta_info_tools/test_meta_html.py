99184c565223c0571727457ca5acba57acaa076a
Fawzi Mohamed <fawzi@kitabi.eu>
2019-11-27 21:43:59 +0100

    generate site of embedded meta schema in meta_html unittests

--- meta_info_tools/test_meta_html.py
+++ meta_info_tools/test_meta_html.py
@@ -3,12 +3,14 @@ from .meta_html import *
 from .test_meta_schema import metaMetaSchema
 import io
 import json
+import tempfile, shutil
 
 
 class TestMetaHtml(unittest.TestCase):
     """tests the documentation generation"""
 
     def test_md2html(self):
+        "tests markdown to html conversion (and math and meta info linking)"
         self.maxDiff = None
         test1 = [
             "The shape of the multidimensional array used to store the data corresponding to this meta info, either ",
@@ -24,6 +26,17 @@ class TestMetaHtml(unittest.TestCase):
             r1,
             '<p>The shape of the multidimensional array used to store the data corresponding to this meta info, either <a href="../section/meta_dimension/value/meta_dimension_fixed.html">meta_dimension_fixed</a> or <a href="../section/meta_dimension/value/meta_dimension_symbolic.html">meta_dimension_symbolic</a>.\nExample: [{ <code><a href="../section/meta_dimension/value/meta_dimension_symbolic.html">meta_dimension_symbolic</a>\'\':</code>number_of_atoms\'\'}, {``<a href="../section/meta_dimension/value/meta_dimension_fixed.html">meta_dimension_fixed</a>\'\': 3 }].\nIf no <a href="../section/meta_dimension/index.html">meta_dimension</a> are given the data is a scalar.</p>',
         )
+
+    def test_site_writer(self):
+        "tests SiteWriter (currently just that it runs)"
+        tempDir = tempfile.mkdtemp(suffix="testSiteWriter")
+        schema = metaMetaSchema()
+        try:
+            w = SiteWriter(schema, tempDir)
+            w.writeAll()
+            w.cleanupUnknown()
+        finally:
+            shutil.rmtree(tempDir)
 
 
 if __name__ == "__main__":
